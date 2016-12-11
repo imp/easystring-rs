afeb8d67e840cd177439a2fc7df55c0a42f25499
Cyril Plisko <cyril.plisko@mountall.com>
2016-12-11 10:36:44 +0200

    Boilerplate PartialEq

--- src/text.rs
+++ src/text.rs
@@ -1,7 +1,7 @@
 use std::convert::From;
 use std::fmt;
 use std::ops::{Add, AddAssign, Deref};
-use std::ffi::{CString, OsString};
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 use super::content::{Content, ToContent};
@@ -11,12 +11,6 @@ pub struct Text {
     text: Vec<Content>,
 }
 
-// impl From<String> for Text {
-//     fn from(s: String) -> Self {
-//         Text { text: vec![Content::String(s)] }
-//     }
-// }
-
 impl<T> From<T> for Text
     where T: ToContent<T>
 {
@@ -24,60 +18,6 @@ impl<T> From<T> for Text
         Text { text: vec![Content::from(s)] }
     }
 }
-
-// impl From<OsString> for Text {
-//     fn from(s: OsString) -> Self {
-//         Text { text: vec![Content::OsString(s)] }
-//     }
-// }
-//
-// impl From<CString> for Text {
-//     fn from(s: CString) -> Self {
-//         Text { text: vec![Content::CString(s)] }
-//     }
-// }
-//
-// impl<T: Into<PathBuf>> From<T> for Text {
-//     fn from(s: PathBuf) -> Self {
-//         Text { text: vec![Content::PathBuf(s)] }
-//     }
-// }
-
-// pub trait Textable<T> {
-//     fn to_text(&self) -> Text;
-// }
-//
-// impl<T> Textable<T> for T
-//     where T: ToContent<T>
-// {
-//     fn to_text(&self) -> Text {
-//         Text::from(self)
-//     }
-// }
-
-// impl<'a> Textable for &'a str {
-//     fn to_text(self) -> Text {
-//         Text { text: self.to_owned() }
-//     }
-// }
-//
-// impl Textable for String {
-//     fn to_text(self) -> Text {
-//         Text { text: self }
-//     }
-// }
-//
-// impl<'a> Textable for &'a Path {
-//     fn to_text(self) -> Text {
-//         Text { text: self.to_string_lossy().into_owned() }
-//     }
-// }
-//
-// impl Textable for PathBuf {
-//     fn to_text(self) -> Text {
-//         Text { text: self.to_string_lossy().into_owned() }
-//     }
-// }
 
 impl Text {
     pub fn new() -> Self {
@@ -104,16 +44,16 @@ impl fmt::Display for Text {
 }
 
 impl Deref for Text {
-    type Target = str;
-    fn deref(&self) -> &str {
+    type Target = OsStr;
+    fn deref(&self) -> &OsStr {
         if self.is_empty() {
-            ""
+            OsStr::new("")
         } else {
             match self.text[0] {
-                Content::String(ref s) => s,
-                Content::OsString(ref s) => s.to_str().unwrap_or(""),
-                Content::CString(ref s) => s.to_str().unwrap_or(""),
-                Content::PathBuf(ref p) => p.to_str().unwrap_or(""),
+                Content::String(ref s) => s.as_ref(),
+                Content::OsString(ref s) => s,
+                Content::CString(ref s) => unimplemented!(),
+                Content::PathBuf(ref p) => p.as_ref(),
             }
         }
     }
@@ -139,15 +79,16 @@ impl Add for Text {
     }
 }
 
-
-// impl<T> Add<T> for Text
-//     where T: Textable
-// {
-//     type Output = Text;
-//     fn add(self, rhs: T) -> Self::Output {
-//         Text { text: self.text + &rhs.to_text() }
-//     }
-// }
+impl<T> Add<T> for Text
+    where T: ToContent<T>
+{
+    type Output = Text;
+    fn add(self, rhs: T) -> Self::Output {
+        let mut text = self.text.clone();
+        text.push(Content::from(rhs));
+        Text { text: text }
+    }
+}
 
 // AddAssign implementation
 
@@ -159,71 +100,66 @@ impl AddAssign for Text {
 
 // PartialEq implementation
 
-impl<T: ?Sized + AsRef<str>> PartialEq<T> for Text {
-    default fn eq(&self, other: &T) -> bool {
+// impl<T: Borrow<OsStr> + ?Sized> PartialEq<T> for Text {
+//     default fn eq(&self, other: &T) -> bool {
+//         unimplemented!()
+//     }
+// }
+
+// impl<T: Borrow<Path> + ?Sized> PartialEq<T> for Text {
+//     default fn eq(&self, other: &T) -> bool {
+//         unimplemented!()
+//     }
+// }
+
+impl PartialEq<str> for Text {
+    fn eq(&self, other: &str) -> bool {
         unimplemented!()
     }
 }
 
-// impl PartialEq<str> for Text {
-//     fn eq(&self, other: &str) -> bool {
-//         *self == Text::from(other)
-//     }
-// }
+impl<'a> PartialEq<&'a str> for Text {
+    fn eq(&self, other: &&'a str) -> bool {
+        unimplemented!()
+    }
+}
 
-// impl<'a> PartialEq<&'a str> for Text {
-//     fn eq(&self, other: &&'a str) -> bool {
-//         self.text == *other
-//     }
-// }
-//
-// impl PartialEq<String> for Text {
-//     fn eq(&self, other: &String) -> bool {
-//         &self.text == other
-//     }
-// }
-//
-// impl<'a> PartialEq<String> for &'a Text {
-//     fn eq(&self, other: &String) -> bool {
-//         self.text == *other
-//     }
-// }
-//
-// impl PartialEq<Path> for Text {
-//     fn eq(&self, other: &Path) -> bool {
-//         match other.to_str() {
-//             Some(s) => self.text == s,
-//             None => false,
-//         }
-//     }
-// }
+impl PartialEq<String> for Text {
+    fn eq(&self, other: &String) -> bool {
+        unimplemented!()
+    }
+}
 
-// impl<'a> PartialEq<&'a Path> for Text {
-//     fn eq(&self, other: &&'a Path) -> bool {
-//         match other.to_str() {
-//             Some(s) => self.text == s,
-//             None => false,
-//         }
-//     }
-// }
+impl<'a> PartialEq<String> for &'a Text {
+    fn eq(&self, other: &String) -> bool {
+        unimplemented!()
+        // self.text == *other
+    }
+}
 
-// impl PartialEq<PathBuf> for Text {
-//     fn eq(&self, other: &PathBuf) -> bool {
-//         match other.to_str() {
-//             Some(s) => self.text == s,
-//             None => false,
-//         }
-//     }
-// }
+impl PartialEq<Path> for Text {
+    fn eq(&self, other: &Path) -> bool {
+        unimplemented!()
+    }
+}
 
-// impl<'a> PartialEq<PathBuf> for &'a Text {
-//     fn eq(&self, other: &PathBuf) -> bool {
-//         match other.to_str() {
-//             Some(s) => self.text == s,
-//             None => false,
-//         }
-//     }
-// }
+impl<'a> PartialEq<&'a Path> for Text {
+    fn eq(&self, other: &&'a Path) -> bool {
+        unimplemented!()
+    }
+}
+
+impl PartialEq<PathBuf> for Text {
+    fn eq(&self, other: &PathBuf) -> bool {
+        unimplemented!()
+    }
+}
+
+impl<'a> PartialEq<PathBuf> for &'a Text {
+    fn eq(&self, other: &PathBuf) -> bool {
+        unimplemented!()
+    }
+}
 
 // impl<T> From<T> for Text
 //     where T: Textable
@@ -233,14 +169,14 @@ impl<T: ?Sized + AsRef<str>> PartialEq<T> for Text {
 //     }
 // }
 
-// impl From<Text> for String {
-//     fn from(t: Text) -> Self {
-//         t.text
-//     }
-// }
-//
-// impl<'a> From<&'a Text> for &'a Path {
-//     fn from(t: &'a Text) -> Self {
-//         Path::new(&t.text)
-//     }
-// }
+impl From<Text> for String {
+    fn from(t: Text) -> Self {
+        unimplemented!()
+    }
+}
+
+impl<'a> From<&'a Text> for &'a Path {
+    fn from(t: &'a Text) -> Self {
+        unimplemented!()
+    }
+}
